f6e75eaed4f763c64d3c2bad671fe6c9b0704935
Dylan Hearn <dylanhearn123@gmail.com>
2023-12-04 05:38:06 +0000

    added working filter button for property tags

--- src/components/discover/properties.tsx
+++ src/components/discover/properties.tsx
@@ -8,10 +8,12 @@ interface Property {
   id: number;
   desc: string;
   image: string;
+  tags: Array<string>;
 }
 
 const Properties = () => {
   const [data, setData] = useState<Property[]>([]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     getData();
@@ -22,30 +24,69 @@ const Properties = () => {
       const responce = await fetch('http://localhost:3000');
       const properties = await responce.json();
       setData(properties);
+      console.log(properties);
     } catch {
       console.log('error on the server');
     }
   }
 
+  // filter results
+
+  const filteredData = filter
+    ? data.filter((item) => item.tags.includes(filter))
+    : data;
+
   return (
     <div className={shared.container}>
-      {data.length > 0 && (
-        <div className={styles.container}>
-          {data.map((item) => (
-            <div key={item.id} className={styles.card}>
-              <img
-                src={item.image}
-                alt="picture of property"
-                className={styles.image}
-              />
-              <div className={styles.content}>
-                <h1 className={styles.heading}>{item.title}</h1>
-                <p className={styles.desc}>{item.desc}</p>
-                <Button text="Show me" />
+      <label htmlFor="filter">
+        <select
+          name="options"
+          id="filter"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="garage">Garage</option>
+          <option value="test">Test</option>
+          <option value="bungalow">Bungalow</option>
+        </select>
+      </label>
+      {filteredData.length > 0 ? (
+        <>
+          <div className={styles.container}>
+            {filteredData.map((item) => (
+              <div key={item.id} className={styles.card}>
+                <img
+                  src={item.image}
+                  alt="picture of property"
+                  className={styles.image}
+                />
+                <div className={styles.content}>
+                  <h1 className={styles.heading}>{item.title}</h1>
+                  <p className={styles.desc}>{item.desc}</p>
+                  <div className={styles.info}>
+                    {item.tags.map((item, ind) => (
+                      <p className={styles.tags} key={ind}>
+                        {item}
+                      </p>
+                    ))}
+                  </div>
+                  <Button text="Show me" />
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <>
+          {data.length ? (
+            <p>
+              No properties found with the <strong>{filter}</strong> tag
+            </p>
+          ) : (
+            <p>No properties avaliable right now</p>
+          )}
+        </>
       )}
     </div>
   );
