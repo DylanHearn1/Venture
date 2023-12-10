7165e07aa98374f327b524f1edbd3e992aa4b882
Dylan Hearn <dylanhearn123@gmail.com>
2023-12-10 06:41:24 +0000

    re-style contact form ui and ux

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,34 +1,29 @@
 import shared from '../sharedComponents/shared.module.scss';
-import './contact.module.scss';
+import './contact.scss';
 
 const Contact = () => {
   return (
     <div className={shared.background}>
       <div className={shared.container}>
         <h1 className={shared.headingwhite}>Get in touch</h1>
-        <p className={shared.paragraphwhite}>
-          Get in touch with our team to learn more about our services and how we
-          can elevate your property sales.
-        </p>
-        <form action="">
-          <label htmlFor="">Your email</label>
-          <input
-            type="email"
-            name=""
-            id=""
-            placeholder="example@gmail.com"
-            required
-          />
-          <label htmlFor="">Your message</label>
-          <textarea
-            id="message"
-            cols={70}
-            maxLength={500}
-            rows={10}
-            placeholder="Your message"
-          ></textarea>
-          <input type="submit" name="" />
-        </form>
+        <div className="contact">
+          <p className={shared.paragraphwhite}>
+            Get in touch with our team to learn more about our services and how
+            we can elevate your property sales.
+          </p>
+          <form action="">
+            <input type="text" name="" id="" placeholder="name" />
+            <input type="email" name="" id="" placeholder="email" />
+            <textarea
+              name=""
+              id=""
+              cols={30}
+              rows={3}
+              placeholder="message"
+            ></textarea>
+            <input type="submit" className="submit" />
+          </form>
+        </div>
       </div>
     </div>
   );
