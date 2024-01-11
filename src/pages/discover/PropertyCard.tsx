2476f898e84673ffed0b9c4b15a841bd7dcc8bbf
Dylan Hearn <dylanhearn123@gmail.com>
2024-01-11 06:06:47 +0000

    feature: loading skeleton added to property cards

---
+++ src/pages/discover/PropertyCard.tsx
@@ -0,0 +1,56 @@
+import Button from '../../components/sharedComponents/Button';
+import styles from './property.module.scss';
+import { Skeleton } from '@mui/material';
+
+interface PropertyCardProps {
+  id: string;
+  image: string;
+  title: string;
+  desc: string;
+  tags: Array<string>;
+  isLoading: boolean;
+}
+
+const PropertyCard = ({
+  id,
+  image,
+  title,
+  desc,
+  tags,
+  isLoading,
+}: PropertyCardProps) => {
+  return (
+    <>
+      {isLoading ? (
+        <div className={styles.skeleton}>
+          <Skeleton variant="rounded" width={350} height={450} />
+        </div>
+      ) : (
+        <div className={styles.card}>
+          <img
+            loading="lazy"
+            src={image}
+            alt="picture of property"
+            className={styles.image}
+          />
+          <div className={styles.content}>
+            <h1 className={styles.heading}>{title}</h1>
+            <p className={styles.desc}>{desc}</p>
+            <div className={styles.info}>
+              {tags.map((tag, ind) => (
+                <p className={styles.tags} key={ind}>
+                  {tag.toUpperCase()}
+                </p>
+              ))}
+            </div>
+            <a href={id} className={styles.button}>
+              <Button text="Show me" />
+            </a>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default PropertyCard;
--- src/pages/discover/properties.tsx
+++ src/pages/discover/properties.tsx
@@ -1,7 +1,7 @@
 import styles from './property.module.scss';
 import { useEffect, useState } from 'react';
 import shared from '../../Components/sharedComponents/shared.module.scss';
-import Button from '../../components/sharedComponents/Button';
+import PropertyCard from './PropertyCard';
 
 interface Property {
   title: string;
@@ -41,67 +41,49 @@ const Properties = () => {
 
   return (
     <div className={shared.container}>
-      {loading ? (
-        <img src="./loading.gif" alt="loading" className={styles.loading} />
-      ) : (
-        <div>
-          <label htmlFor="filter">
-            <select
-              name="options"
-              id="filter"
-              value={filter}
-              onChange={(e) => setFilter(e.target.value)}
-            >
-              <option value="">All</option>
-              <option value="sold">Sold</option>
-              <option value="listed">Listed</option>
-              <option value="to let">To let</option>
-            </select>
-          </label>
-          {filteredData.length > 0 ? (
-            <>
-              <div className={styles.container}>
-                {filteredData.map((item) => (
-                  <div key={item.id} className={styles.card}>
-                    <img
-                      loading="lazy"
-                      src={item.image}
-                      alt="picture of property"
-                      className={styles.image}
-                    />
-                    <div className={styles.content}>
-                      <h1 className={styles.heading}>{item.title}</h1>
-                      <p className={styles.desc}>{item.desc}</p>
-                      <div className={styles.info}>
-                        {item.tags.map((item, ind) => (
-                          <p className={styles.tags} key={ind}>
-                            {item.toUpperCase()}
-                          </p>
-                        ))}
-                      </div>
-                      <a href={item.id} className={styles.button}>
-                        <Button text="Show me" />
-                      </a>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </>
-          ) : (
-            <>
-              {data.length ? (
-                <p className={shared.paragraph}>
-                  No properties found with the <strong>{filter}</strong> tag.
-                </p>
-              ) : (
-                <p className={shared.paragraph}>
-                  No properties avaliable right now.
-                </p>
-              )}
-            </>
-          )}
-        </div>
-      )}
+      <div>
+        <label htmlFor="filter">
+          <select
+            name="options"
+            id="filter"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="sold">Sold</option>
+            <option value="listed">Listed</option>
+            <option value="to let">To let</option>
+          </select>
+        </label>
+        {filteredData.length > 0 ? (
+          <>
+            <div className={styles.container}>
+              {filteredData.map((item) => (
+                <PropertyCard
+                  id={item.id}
+                  title={item.title}
+                  image={item.image}
+                  desc={item.desc}
+                  tags={item.tags}
+                  isLoading={loading}
+                />
+              ))}
+            </div>
+          </>
+        ) : (
+          <>
+            {data.length ? (
+              <p className={shared.paragraph}>
+                No properties found with the <strong>{filter}</strong> tag.
+              </p>
+            ) : (
+              <p className={shared.paragraph}>
+                No properties avaliable right now.
+              </p>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
